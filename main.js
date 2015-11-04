29b3cd43151e8048480a2fedf3407f907f8993ed
Max Thomas <maxamadeothomas@gmail.com>
2015-11-04 14:46:35 -0500

    act 2 complete loanin like a champpp

--- main.js
+++ main.js
@@ -10,13 +10,19 @@ $(document).ready(function(){
 			$createLi.text($value);
 			$('#log').append($createLi);
 			$("#prompt").val("");
+			$valueArray=$value.split(" ")
+			console.log($valueArray)
 			var respond=function(response){
 				$createLiResponse=$('<li>');
 				$createLiResponse.text(response)
 				$('#log').append($createLiResponse);}
 			if ($value==='purse'){
 				return respond('*ShylockBot pulls out his purse containing '+ducats+' ducats*')
+			} else if ($valueArray[0]==='loan'){
+				ducats-=parseInt($valueArray[2])
+				return respond('*ShylockBot gives '+$valueArray[1]+' '+$valueArray[2]+' ducats*')
 			}
+
 		}
 	};
 	var $input = $('inputBox')
